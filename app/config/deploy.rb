414ce9713dfcf96ca0ddba4563063d1d8cd4e584
evgen <evgen.kovalevich@gmail.com>
2015-02-08 14:13:14 +0300

    Настроены vk, fb auth

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "set your application name here"
 set :domain,      "88.208.26.26"
-set :user,        "evgen"
+set :user,        "root"
 set :use_sudo,    false
 set :deploy_to,   "/home/evgen/autorinok.by"
 set :app_path,    "app"
@@ -8,16 +8,14 @@ set :app_path,    "app"
 set :repository,  "file:///home/evgen/sites/auto/auto"
 set :deploy_via,  :copy
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
-# Or: `propel`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
 set :shared_files, ["app/config/parameters.yml"]
-set :shared_children, [app_path + "/logs", web_path + "/uploads"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
 set :use_composer, true
 set :update_vendors, true
 
